e002d3edab3ec92dc183a11b806f827307c0c7d2
Suytry Sok <soksoytry2000@gmail.com>
2026-06-14 19:53:36 +0700

    become_an_author

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  // apiUrl: 'http://localhost:8080/api',
-  apiUrl: 'https://ef9d-175-100-48-40.ngrok-free.app/api',
+  apiUrl: 'http://localhost:8080/api',
+  // apiUrl: 'https://555e-203-144-76-179.ngrok-free.app/api',
 };
